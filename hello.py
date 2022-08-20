820fd74e3d18a690212e9c0b96f6de05657bcf64
SeojungH <chseojung@naver.com>
2022-08-20 22:48:17 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+
+a = 3
+print(type(a))
+print(a)
+
+b = 4
+print(type(b))
+print(a+b)
+print(a/b) #나누기
+print(a//b) #나눈 몫
+print(a*b) #곱하기
+print(a**b) #제곱
+
+c = "seojung"
+
+print(type(c)) #str으로 나옴
