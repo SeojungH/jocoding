446aa1aafc2b9ed8e5e596cf31b185bedd11e16e
SeojungH <chseojung@naver.com>
2022-08-26 00:05:12 +0900

    Create list.py

---
+++ list.py
@@ -0,0 +1,18 @@
+a = [1,2, "my", "name", ["is", "babo"]]
+
+print(a[4][1])
+
+b = [1,2,3]
+print(b[0])
+
+c = [1, 2, 3, 4, 5]
+print(c[0:2])
+
+d = ['a', 'c', 'b']
+d.sort()
+print(d)
+
+
+e = [1,2,3]
+e.pop()
+print(e)
